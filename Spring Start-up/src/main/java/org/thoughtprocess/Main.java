425593257d7e6ee358da05ea0171c87f47f04324
Fayaad Abrahams <Fayaad.Abrahams@digiblu.com>
2024-02-06 19:32:56 +0200

    Added basic file structure.
    - Added dependencies, as well as the Primary config class (main)

---
+++ Spring Start-up/src/main/java/org/thoughtprocess/Main.java
@@ -0,0 +1,11 @@
+package org.thoughtprocess;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Primary application config class
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
